83c8deef3b6fab77f471a396637bd349a933bc48
Ramiyan Gangatharan <ramiyan2185@gmail.com>
2024-01-20 09:53:10 -0500

    ICE 1

--- Scripts/app.js
+++ Scripts/app.js
@@ -9,38 +9,38 @@
         {
             console.log("Called DisplayHomePage");
 
+            let MainContent = document.getElementsByTagName("main")[0];
+            let MainParagraph = document.createElement("p");
+
+            MainParagraph.setAttribute("id", "MainParagraph");
+            MainParagraph.setAttribute("class", "mt-3");
+            MainParagraph.textContent = "This is my first paragraph";
+            MainContent.appendChild(MainParagraph);
+
+            let FirstString = "This is";
+
+            // `` is a string literal to concatenate strings.
+
+            let SecondString = `${FirstString} the main Paragraph.`;
+            MainParagraph.textContent = SecondString;
+            MainContent.appendChild(MainParagraph);
+
+            let DocumentBody = document.body;
+
+            let Article = document.createElement("article");
+            let ArticleParagraph
+                = `<p id="ArticleParagraph" class = "mt-3"> This is my article paragraph</p>`;
+
+            Article.setAttribute("class", "container");
+            Article.innerHTML = ArticleParagraph;
+
+            DocumentBody.appendChild(Article);
+
             let AboutUsButton= document.getElementById("AboutUsBtn");
 
             AboutUsButton.addEventListener("click", function ()
             {
                 location.href = "about.html"
-
-                let MainContent = document.getElementsByTagName("main")[0];
-                let MainParagraph = document.createElement("p");
-
-                MainParagraph.setAttribute("id", "MainParagraph");
-                MainParagraph.setAttribute("class", "mt-3");
-                MainParagraph.textContent = "This is my first paragraph";
-                MainContent.appendChild(MainParagraph);
-
-                let FirstString = "This is";
-
-                // `` is a string literal to concatenate strings.
-
-                let SecondString = `${FirstString} the main Paragraph.`;
-                MainParagraph.textContent = SecondString;
-                MainContent.appendChild(MainParagraph);
-
-                let DocumentBody = document.body;
-
-                let Article = document.createElement("article");
-                let ArticleParagraph
-                    = `<p id="ArticleParagraph" class = "mt-3"> This is my article paragraph</p>`;
-
-                Article.setAttribute("class", "container");
-                Article.innerHTML = ArticleParagraph;
-
-                DocumentBody.appendChild(Article);
             }
             )
         }
